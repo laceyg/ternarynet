8bbda8909c17d6385e0fbf247fa8bfad6d5b8f85
Griffin Lacey <griffinlacey@gmail.com>
2017-03-02 15:07:38 -0500

    updated setup script

--- scripts/setup_COP.sh
+++ scripts/setup_COP.sh
@@ -6,5 +6,5 @@ module load openmpi/intel1503-std/1.8.7
 module load hdf/serial/5.1.8.11 
 module load python/intel/2.7.10
 export LD_LIBRARY_PATH=/opt/sharcnet/testing/cudnn/cudnn4:$LD_LIBRARY_PATH
-export PYTHONPATH=$PYTHONPATH:/opt/sharcnet/opencv/2.4.9/lib/python2.6/site-packages/:/home/laceyg/Personal/ternarynet
+export PYTHONPATH=$PYTHONPATH:/opt/sharcnet/opencv/2.4.9/lib/python2.6/site-packages/:/home/laceyg/Personal/ternarynet/tools/tensorpack
 export TENSORPACK_DATASET=/tmp/MLRG/ilsvrc12/
